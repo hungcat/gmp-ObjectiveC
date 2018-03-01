aaac46eaec5b88bdda9434b3a9aef35f5fd16c5e
NTT <NTT@Shunichi-no-MacBook-Air.local>
2018-03-02 00:19:29 +0900

    add gmp

--- gmp-ObjectiveC/ViewController.m
+++ gmp-ObjectiveC/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <libgmp/gmp.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Hello World!");
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+    
+    mpz_t a;
+    mpz_init(a); // 初期化
+    // 文字列の形で任意精度整数をセットする
+    mpz_set_str(a, "48410242354393281104234213124421033", 10);
+    //mpz_out_str(stdout, 10, a); // 出力用関数
+    [label setText:@(mpz_get_str(NULL, 10, a))];
+    // 後処理
+    mpz_clear(a);
+    
+    [label setLineBreakMode:NSLineBreakByWordWrapping];
+    [label setNumberOfLines:0];
+    [label setFrame:CGRectMake(0, 0, self.view.frame.size.width, label.frame.size.height)];
+    [label sizeToFit];
+    [label setCenter:self.view.center];
+    [self.view addSubview:label];
 }
 
 
